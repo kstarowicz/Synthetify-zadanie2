ed08540269915911146161063e701a8bc36f0029
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2023-08-16 14:28:33 +0200

    2 dodanie

--- zadanie.2/escrow_contract/escrow_contract/src/lib.rs
+++ zadanie.2/escrow_contract/escrow_contract/src/lib.rs
@@ -1,4 +1,5 @@
 #![no_std]
+use casper_psp22::PSP22;
 
 extern crate alloc;
 
@@ -13,7 +14,8 @@ use casper_types::{
 
 pub struct Escrow {
     owner: PublicKey,
-    funds: U512,
+    token_contract: ContractHash,
+    swap_token_contract: ContractHash,
 }
 
 impl Escrow {
@@ -29,7 +31,10 @@ impl Escrow {
         if caller != self.owner {
             runtime::revert(Error::NotAllowed);
         }
-        self.funds += amount;
+        
+        // Wysyłanie tokenów do kontraktu
+        let token = PSP22::at_address(self.token_contract);
+        token.transfer_from(caller, runtime::get_key("escrow_contract").into(), amount);
     }
 
     pub fn withdraw(&mut self, amount: U512) {
@@ -37,13 +42,16 @@ impl Escrow {
         if caller != self.owner {
             runtime::revert(Error::NotAllowed);
         }
-        if self.funds < amount {
+    
+        let token = PSP22::at_address(self.token_contract);
+        let balance = token.balance_of(runtime::get_key("escrow_contract").into());
+        if balance < amount {
             runtime::revert(Error::InsufficientFunds);
         }
-        self.funds -= amount;
-        // brak logiki do wysyłania funduszy do właścicieli
+        
+        // Wysyłanie tokenów z kontraktu
+        token.transfer(caller, amount);
     }
-}
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum Error {
@@ -59,3 +67,48 @@ impl From<Error> for u16 {
         }
     }
 }
+
+
+let swap_entry_point = EntryPoint::new(
+    String::from("swap"),
+    vec![
+        Parameter::new("amount", CLType::U512),
+        Parameter::new("swap_rate", CLType::U512),
+    ],
+    CLType::Unit,
+    EntryPointAccess::Public,
+    EntryPointType::Contract,
+);
+
+
+pub fn swap(&mut self, amount: U512, swap_rate: U512) {
+    let caller = runtime::get_caller();
+    if caller != self.owner {
+        runtime::revert(Error::NotAllowed);
+    }
+
+    // Sprawdzenie czy kontrakt ma wystarczającą ilość tokenów do wymiany
+    let token = PSP22::at_address(self.token_contract);
+    let balance = token.balance_of(runtime::get_key("escrow_contract").into());
+    if balance < amount {
+        runtime::revert(Error::InsufficientFunds);
+    }
+    
+    // Odbieranie tokenów od właściciela
+    token.transfer_from(caller, runtime::get_key("escrow_contract").into(), amount);
+
+    // Obliczenie ilości tokenów do wymiany
+    let swap_amount = amount * swap_rate;
+
+    // Wysyłanie tokenów swap na adres właściciela
+    let swap_token = PSP22::at_address(self.swap_token_contract);
+    swap_token.transfer(caller, swap_amount);
+}
+
+
+
+
+
+
+
+//testy jednostkowe
